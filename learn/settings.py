852bbd010f4cf16881a673eb89079aee159d0c48
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-04-04 21:57:08 +0530

    Deploying on Heroku

--- learn/settings.py
+++ learn/settings.py
@@ -133,9 +133,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATIC_ROOT = 'static'
 
