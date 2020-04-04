97ce3eb3fcce3e5bdcb090f6c8197951a8499fac
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-04-04 18:15:20 +0530

    Server Successfully Exectuted

--- contact/urls.py
+++ contact/urls.py
@@ -10,7 +10,7 @@ from contact.views import (
 app_name = 'contact'
 
 urlpatterns = [
-    path('', home, name='home']),
+    path('', home, name='home'),
     path('enroll/step-1/', enroll, name='enroll'),
     path('enroll/step-2/', social, name='social'),
     path('enroll/success/waiting/', thanks, name='thanks'),
