af3b81c197a7fc9f02d82b9b911f8ab4bdb40d96
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-04-04 22:03:17 +0530

    Deploying on Heroku

--- learn/settings.py
+++ learn/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
