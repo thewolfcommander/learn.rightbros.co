223ee2197c691c5fb2c5037d8f71465c87bc4337
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-04-04 18:26:37 +0530

    Styling

--- learn/urls.py
+++ learn/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('demo/', include('contact.urls')),
+    path('', include('contact.urls')),
 ]
